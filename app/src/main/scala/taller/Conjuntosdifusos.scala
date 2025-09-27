3bc151bae51eaeba6ba793d905ed5d5b80da6c7f
SebasG111 <sebastian.garcia.cardona@correounivalle.edu.co>
2025-09-27 11:04:03 -0500

    Se agregan los metodos complemento, union e interseccion

--- app/src/main/scala/taller/Conjuntosdifusos.scala
+++ app/src/main/scala/taller/Conjuntosdifusos.scala
@@ -25,4 +25,31 @@ class Conjuntosdifusos {
 
 
 
+  def complemento(c: ConjuntoDifuso): ConjuntoDifuso = {
+    def definec(x:Int): Double = {
+      1-c(x)
+    }
+    definec
+  }
+
+
+
+  def union(cd1: ConjuntoDifuso, cd2: ConjuntoDifuso): ConjuntoDifuso = {
+    def defineu(x: Int): Double = {
+      math.max(cd1(x),cd2(x))
+    }
+    defineu
+  }
+
+
+
+  def interseccion(cd1: ConjuntoDifuso, cd2: ConjuntoDifuso): ConjuntoDifuso = {
+    def definein(x: Int): Double = {
+      math.min(cd1(x),cd2(x))
+    }
+    definein
+  }
+
+
+
 }
